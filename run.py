f295af3f5a1f727056c7eb09672f9b442215d1f8
Robert Grenyer <robertgrenyer@btinternet.com>
2024-09-06 16:17:50 +0000

    Add Jinja url_for templating method to nav links

--- run.py
+++ run.py
@@ -5,9 +5,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/") # Browse to the root directory, then Flask triggers the function beneath.
+@app.route("/")
+# Browse to the root directory, then Flask triggers the function beneath.
+# The root decorator binds the index() function to itself, thus whenever that
+# root is called, the function is called. This function is also called a 'view'.
 def index():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__": # __main__ is the name of the default module in Python
